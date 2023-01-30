9f230a8b77217972aeb052be713abf441e945a1c
liyeol <98913681+liyeol@users.noreply.github.com>
2023-01-30 19:46:18 +0000

    ver1

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackClass
+{
+    class Stack<T>
+    {
+        private List<T> stack = new List<T>();
+
+        public int Count
+        {
+            get { return stack.Count; }
+        }
+
+        public void Push(T obj)
+        {
+            stack.Add(obj);
+        }
+
+        public T Pop()
+        {
+            T obj = stack[stack.Count - 1];
+            stack.RemoveAt(stack.Count - 1);
+            return obj;
+        }
+
+        public T Peek()
+        {
+            return stack[stack.Count - 1];
+        }
+
+        public void Clear()
+        {
+            stack.Clear();
+        }
+
+        public void CopyTo(T[] arr)
+        {
+            stack.CopyTo(arr);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine("Count: " + stack.Count);
+            Console.WriteLine("Peek: " + stack.Peek());
+            Console.WriteLine("Pop: " + stack.Pop());
+            Console.WriteLine("Count: " + stack.Count);
+
+            int[] arr = new int[stack.Count];
+            stack.CopyTo(arr);
+            Console.WriteLine("Array: " + string.Join(", ", arr));
+
+            stack.Clear();
+            Console.WriteLine("Count: " + stack.Count);
+        }
+    }
+}
